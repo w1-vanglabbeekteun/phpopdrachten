d945211f68e58b996f36be70867b399ba9acbf98
Teun van Glabbeek <teun.vanglabbeek@edu-kw1c.nl>
2020-02-16 18:01:44 +0100

    Opdracht 2.1

---
+++ css/opdracht2.1.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * User: Teun van Glabbeek
+ * Date: 15/02/2002
+ * Time: 17:25
+ * File: index.php
+ */
+?>
+
+<body>
+<header>
+    <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+</header>
+<aside>
+    <h2>Menu</h2>
+    <ul>
+        <li>Hoofdstuk
+            <ul>
+                <li>
+                    <a href="hoofdstuk2/opdracht_2-1.php">Opdracht 2.1</a>
+                </li>
+                <li>
+                    <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 2.2</a>
+                </li>
+                <p >echo "Hello world"</p>
+            </ul>
+        </li>
+    </ul>
+</aside>
+<main id="wrapper">
+    <h2>Uitwerkingen</h2>
+</main>
