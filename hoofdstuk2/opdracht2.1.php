a4051acb01f7723ae832704c42d18fd74cd48e32
Teun van Glabbeek <teun.vanglabbeek@edu-kw1c.nl>
2020-02-17 12:28:22 +0100

    Opdracht 2.1 gefixt door de docent

---
+++ hoofdstuk2/opdracht2.1.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * User: Teun van Glabbeek
+ * Date: 15/02/2002
+ * Time: 17:25
+ * File: index.php
+ */
+?>
+<!doctype html>
+<head>
+    <title>Uitwerking</title>
+</head>
+<body>
+<header>
+    <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+</header>
+<main id="wrapper">
+    <h2>Uitwerkingen</h2>
+    <p>
+    <?php echo "Hello world"; ?>
+    </p>
+    <a href="../index.php">Terug</a>
+</main>
+</body>
+</html>
--- index.php
+++ index.php
@@ -17,10 +17,10 @@
             <li>Hoofdstuk
                 <ul>
                     <li>
-                        <a href="hoofdstuk2/opdracht_2-1.php">Opdracht 2.1</a>
+                        <a href="hoofdstuk2/opdracht2.1.php">Opdracht 2.1</a>
                     </li>
                     <li>
-                        <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 2.2</a>
+                        <a href="hoofdstuk2/opdracht2.2.php">Opdracht 2.2</a>
                     </li>
                 </ul>
             </li>
