8c6f116f16c74a6c0d799d6711c570e21c647a30
Teun van Glabbeek <teun.vanglabbeek@edu-kw1c.nl>
2020-03-01 21:20:13 +0100

    Opdracht 3.1

---
+++ hoofdstuk3/opdracht3.1.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * User: Teun van Glabbeek
+ * Date: 1-3-2020
+ * Time: 18:56
+ * File: opdracht3.1.php
+ */
+?>
+
+<!DOCTYPE html>
+
+<html lang="nl">
+<head>
+    <title>
+
+    </title>
+
+    <meta name="description" content=" ">
+    <meta name="keywords" content=" ">
+    <meta charset="utf-8">
+    <link href="CSS/style.css" type="text/css" rel="stylesheet">
+</head>
+
+<body>
+<h2>Uitwerking van <a href="../index.php"> PHP-opdrachten</a></h2>
+<div class="box">
+    <?php
+    $elf = "Elfstedentocht";
+    $alve = "Alvestêdetocht";
+    $km = "200";
+    $schaats = "schaatstocht";
+    $natuur = "natuurijs";
+    $kvdfes = "Koninklijke Vereniging De Friesche Elf Steden";
+    $leeuw = "Leeuwarden";
+    $fries = "Friesland";
+    $num = "15";
+    $jaar = "1909";
+    $verhaal = "De $elf (Fries: $alve) is een $km
+                kilometer lange $schaats over $natuur die wordt
+                georganiseerd door de $kvdfes. $leeuw, de hoofdstad van $fries, is start- en
+                aankomstplaats. De $elf is inmiddels $num maal
+                verreden en werd voor het eerst in $jaar gereden en wordt
+                maximaal 1 keer per winter gehouden.";
+    echo ("$verhaal");
+    ?>
+</div>
+</body>
+</html>
