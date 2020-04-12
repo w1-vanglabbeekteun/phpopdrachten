c5df7d74291d65419dc209b180812b84126b0adc
Teun van Glabbeek <teun.vanglabbeek@edu-kw1c.nl>
2020-04-12 23:25:08 +0200

    index

--- index.php
+++ index.php
@@ -20,7 +20,25 @@
                         <a href="hoofdstuk2/opdracht2.1.php">Opdracht 2.1</a>
                     </li>
                     <li>
-                        <a href="hoofdstuk2/opdracht2.2.php">Opdracht 2.2</a>
+                        <a href="hoofdstuk3/opdracht3.1.php">Opdracht 3.1</a>
+                    </li>
+                    <li>
+                        <a href="hoofdstuk3.2/opdracht3.2.php">Opdracht 3.2</a>
+                    </li>
+                    <li>
+                        <a href="hoofdstuk4.3/Opdracht4.3.php">Opdracht 4.3</a>
+                    </li>
+                    <li>
+                        <a href="hoofdstuk4.4/Opdracht4.4.php">Opdracht 4.4</a>
+                    </li>
+                    <li>
+                        <a href="hoofdstuk5.1/opdracht5.1.php">Opdracht 5.1</a>
+                    </li>
+                    <li>
+                        <a href="hoofdstuk5.3/opdracht5.3.php">Opdracht 5.3</a>
+                    </li>
+                    <li>
+                        <a href="hoofdstuk5.4/opdracht5.4.php">Opdracht 5.4</a>
                     </li>
                 </ul>
             </li>
